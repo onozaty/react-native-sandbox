9e30858c9ff92bb8d19ebbb8d1bed029590429fd
onozaty <onozaty@gmail.com>
2023-10-07 00:08:29 +0900

    [udemy] セクション11の89まで

--- udemy/MemoApp/src/screens/SignUpScreen.tsx
+++ udemy/MemoApp/src/screens/SignUpScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 
 import Button from '../components/Button';
@@ -6,12 +6,31 @@ import { useNavigation } from '@react-navigation/native';
 
 export default function SignUpScreen() {
   const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={styles.container}>
       <View style={styles.inner}>
         <Text style={styles.title}>Sign Up</Text>
-        <TextInput style={styles.input} value='Email Address' />
-        <TextInput style={styles.input} value='Password' />
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize='none'
+          keyboardType='email-address'
+          placeholder='Email Address'
+          textContentType='emailAddress'
+        />
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+          autoCapitalize='none'
+          placeholder='Password'
+          secureTextEntry
+          textContentType='password'
+        />
         <Button
           label='Submit'
           onPress={() => {
